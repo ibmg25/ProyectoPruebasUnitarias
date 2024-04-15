5ee327999bbc25758f087aa34fcc108ec12b0cdd
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-04-15 17:39:04 -0400

    pruebas funcion check_winners_helper

--- app/tests/test_calculator.py
+++ app/tests/test_calculator.py
@@ -1,10 +1,9 @@
-from os import getcwd
 from pokercalculator.calculator import Calculator
 
-
-FILE_PATH_SOURCE = getcwd() + "\\" + "pokerdata.txt"
-CALCULATOR = Calculator(FILE_PATH_SOURCE)
+calculator = Calculator("./txts/winner1.txt")
 
 
-def test_calculator():
-    Calculator(FILE_PATH_SOURCE)
+def test_check_winners_helper():
+    assert calculator._Calculator__check_winners_helper(2, 1) == 0
+    assert calculator._Calculator__check_winners_helper(1, 2) == 1
+    assert calculator._Calculator__check_winners_helper(1, 1) == 2
