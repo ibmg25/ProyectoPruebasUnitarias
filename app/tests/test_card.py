94f418b28d1097efbd8b529425af134646b162a2
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-04-15 18:24:53 -0400

    pruebas completas para car.py

--- app/tests/test_card.py
+++ app/tests/test_card.py
@@ -5,3 +5,9 @@ from pokercalculator.suit import Suit
 def test_from_string():
     jack_of_clubs = Card.from_string("JC")
     assert Rank.JACK == jack_of_clubs.rank and Suit.CLUBS == jack_of_clubs.suit
+
+def test_to_string_name():
+    assert "THREE OF HEARTS" == Card(Rank.THREE, Suit.HEARTS).to_string_name()
+
+def test_str():
+    assert "5D" == str(Card(Rank.FIVE, Suit.DIAMONDS))
